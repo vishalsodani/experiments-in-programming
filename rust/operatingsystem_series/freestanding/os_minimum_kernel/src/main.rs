2314a706c58bb0399599d0b96d18c726648159fe
vishalsodani <vishalsodani@rediffmail.com>
2020-07-06 18:53:22 +0530

    successfully created bootimage for QEMu and printed hello world

--- rust/operatingsystem_series/freestanding/os_minimum_kernel/src/main.rs
+++ rust/operatingsystem_series/freestanding/os_minimum_kernel/src/main.rs
@@ -4,6 +4,7 @@
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Hello World!";
 /// This function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -14,5 +15,13 @@ fn panic(_info: &PanicInfo) -> ! {
 pub extern "C" fn _start() -> ! {
     // this function is the entry point, since the linker looks for a function
     // named `_start` by default
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
     loop {}
 }
