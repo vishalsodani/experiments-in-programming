1754a95f365269705eb03444c377b08d50304e17
vishalsodani <vishalsodani@rediffmail.com>
2020-07-07 10:28:03 +0530

    hanlded newline

--- rust/operatingsystem_series/freestanding/vgamode/src/vga_buffer.rs
+++ rust/operatingsystem_series/freestanding/vgamode/src/vga_buffer.rs
@@ -96,7 +96,26 @@ impl Writer {
         }
     }
 
-    fn new_line(&mut self) {/* TODO */}
+    fn new_line(&mut self) {
+        for row in 1..BUFFER_HEIGHT {
+            for col in 0..BUFFER_WIDTH {
+                let character = self.buffer.chars[row][col].read();
+                self.buffer.chars[row - 1][col].write(character);
+            }
+        }
+        self.clear_row(BUFFER_HEIGHT - 1);
+        self.column_position = 0;
+    }
+
+    fn clear_row(&mut self, row: usize) {
+        let blank = ScreenChar {
+            ascii_character: b' ',
+            color_code: self.color_code,
+        };
+        for col in 0..BUFFER_WIDTH {
+            self.buffer.chars[row][col].write(blank);
+        }
+    }
 }
 
 impl fmt::Write for Writer {
@@ -117,5 +136,7 @@ pub fn print_something() {
     writer.write_byte(b'H');
     writer.write_string("ello ");
     write!(writer, "The numbers are {} and {}", 42, 1.0/3.0).unwrap();
+    writer.write_string("ello sfsdfsdfdsfdsfdsfdfafafafafsasafa");
+    writer.write_string("sfslf fls fdsfls fdslf shfs fsf sf");
 }
 
