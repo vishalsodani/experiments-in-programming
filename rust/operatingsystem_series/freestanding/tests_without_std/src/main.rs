4762fca475d9330c5f58fe18be62b5f280158c8e
vishalsodani <vishalsodani@rediffmail.com>
2020-07-13 16:13:19 +0530

    running 0 tests

--- rust/operatingsystem_series/freestanding/tests_without_std/src/main.rs
+++ rust/operatingsystem_series/freestanding/tests_without_std/src/main.rs
@@ -2,6 +2,7 @@
 #![no_main]
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 mod vga_buffer;
@@ -25,7 +26,10 @@ fn test_runner(tests: &[&dyn Fn()]) {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    
+    println!("Hello World{}", "!");
+
+    #[cfg(test)]
+    test_main();
 
     loop {}
 }
