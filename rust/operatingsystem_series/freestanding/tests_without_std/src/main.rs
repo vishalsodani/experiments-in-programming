ee9c06ac25e7c793ed1ddac3d344f45dca847418
vishalsodani <vishalsodani@rediffmail.com>
2020-07-13 16:09:58 +0530

    made working for cargo xtest

--- rust/operatingsystem_series/freestanding/tests_without_std/src/main.rs
+++ rust/operatingsystem_series/freestanding/tests_without_std/src/main.rs
@@ -1,13 +1,27 @@
 #![no_std]
 #![no_main]
-
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
 
 use core::panic::PanicInfo;
+mod vga_buffer;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
+
+
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
